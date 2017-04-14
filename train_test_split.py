e6185d6bb9eeba982a55f87bddda534d1dc94483
rforgione <rforgione@gmail.com>
2017-04-14 01:10:29 -0400

    layer in validation set extraction

--- train_test_split.py
+++ train_test_split.py
@@ -4,6 +4,16 @@
 
 import os
 import re
+import random
+
+if not os.path.exists("../valid"):
+    os.makedirs("../valid")
+
+if not os.path.exists("../valid/dogs"):
+    os.makedirs("../valid/dogs")
+
+if not os.path.exists("../valid/cats"):
+    os.makedirs("../valid/cats")
 
 if not os.path.exists("dogs"):
     os.makedirs("dogs")
@@ -17,10 +27,15 @@ cat_pattern = re.compile("^cat.+\.jpg$")
 
 for file in os.listdir("."):
     if bool(dog_pattern.match(file)):
-        print file
-        print "dogs/" + file
-        os.rename(file, "dogs/" + file)
+        num = random.randint(0,3)
+        if num == 0:
+            os.rename(file, "../valid/dogs/" + file)
+        else:
+            os.rename(file, "dogs/" + file)
     elif bool(cat_pattern.match(file)):
-        os.rename(file, "cats/" + file)
+        if num == 0:
+            os.rename(file, "../valid/cats/" + file)
+        else:
+            os.rename(file, "cats/" + file)
 
 
