320336f9c26d8214b50db5ef52698d29982b6bb8
rforgione <rforgione@gmail.com>
2017-04-14 00:06:11 -0400

    finish script to split images into train and test

--- train_test_split.py
+++ train_test_split.py
@@ -1,6 +1,26 @@
-file_dir = # ...
+# A script for moving the cat and dog image files from the Kaggle
+# competition dataset into the appropriate data structure for the Vgg class.
+# The script should be run from within the directory of image files.
+
+import os
+import re
+
+if not os.path.exists("dogs"):
+    os.makedirs("dogs")
+
+if not os.path.exists("cats"):
+    os.makedirs("cats")
+
 # iterate over files
+dog_pattern = re.compile("^dog.+\.jpg$")
+cat_pattern = re.compile("^cat.+\.jpg$")
 
-# figure out the class of the file based on its name
+for file in os.listdir("."):
+    if bool(dog_pattern.match(file)):
+        print file
+        print "dogs/" + file
+        os.rename(file, "dogs/" + file)
+    elif bool(cat_pattern.match(file)):
+        os.rename(file, "cats/" + file)
 
-# move the file into the directory matching its class name
+
